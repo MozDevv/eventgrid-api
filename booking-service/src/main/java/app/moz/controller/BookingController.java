0da8534a4b0e26ac8acbaf79fa43e5488ad2169f
Mozz <sammymathenge5@gmail.com>
2023-12-07 04:10:55 +0300

    Circuit breaker impl

--- booking-service/src/main/java/app/moz/controller/BookingController.java
+++ booking-service/src/main/java/app/moz/controller/BookingController.java
@@ -4,9 +4,11 @@ package app.moz.controller;
 import app.moz.dto.BookingDto;
 import app.moz.dto.BookingRequest;
 import app.moz.service.BookingService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -31,9 +33,18 @@ public class BookingController {
 
     @PostMapping("/bookings/{userId}/{eventId}")
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "event", fallbackMethod = "fallbackMethod")
     public BookingDto createBooking (@RequestBody BookingRequest bookingRequest,
                                      @PathVariable int userId,
                                      @PathVariable int eventId) {
         return bookingService.createBooking(bookingRequest, userId, eventId);
     }
+
+    public BookingDto fallbackMethod(BookingRequest bookingRequest, int userId, int eventId, Throwable throwable
+    ) {
+        System.out.println("Fallback: Something went wrong with the booking service.");
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking service is unavailable");
+    }
+
 }
