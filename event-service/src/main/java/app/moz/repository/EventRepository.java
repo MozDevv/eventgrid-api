caaa9b9fb85b86c08c4efb69b401248198c1ebef
Mozz <sammymathenge5@gmail.com>
2024-01-12 12:09:40 +0300

    Add booking-app/pom.xml

--- event-service/src/main/java/app/moz/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package app.moz.repository;
-
-import app.moz.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
